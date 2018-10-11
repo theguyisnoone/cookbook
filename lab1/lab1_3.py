cd823e9dc688f2f055f24753f792166dd84d1629
hajaking <953258481@qq.com>
2018-10-11 15:39:14 +0800

    这个程序不太明白
    fix bug

--- lab1/lab1_3.py
+++ lab1/lab1_3.py
@@ -1,12 +1,12 @@
 from  collections import deque
 
-##fgfgfgg
+
 def search(lines,pattern,history=5):
     previous_lines= deque(maxlen=history)
-    for line in lines:
-        for pattern in line:
-            yield line,previous_lines
-        previous_lines.append(line)
+    for li in lines:
+        if  pattern in li:
+            yield li,previous_lines
+        previous_lines.append(li)
 
 if __name__ =="__main__":
     with open(r'somefile.txt') as f:
