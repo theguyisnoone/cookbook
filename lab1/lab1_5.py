837b001e4f070ffb40df8048c27c59ec6f0b20ce
hajaking <953258481@qq.com>
2018-10-11 16:50:36 +0800

    实现一个优先级队列

---
+++ lab1/lab1_5.py
@@ -0,0 +1,36 @@
+import heapq
+class PriorityQueue:
+    def __init__(self):
+        self._queue=[] #null
+        self._index=0#null
+
+    def push(self,item,priority):
+        heapq.heappush(self._queue, (-priority,self._index,item)) #？、
+        self._index+=1
+
+    def pop(self):
+        return  heapq.heappop(self._queue)[-1]#min？
+
+
+class Item:
+    def __init__(self,name):
+        self.name=name
+
+    def __repr__(self):
+        return 'Item({!r})'.format(self.name)
+
+q=PriorityQueue()
+q.push(Item('foo'), 1)
+q.push(Item('bar'), 5)
+q.push(Item('spam'),4)
+q.push(Item('gork'),1)
+print(q.pop())
+# Item('bar')
+print(q.pop())
+# Item('spam')
+print(q.pop())
+# Item('foo')
+print(q.pop())
+# Item('gork')
+
+print(q.pop())
