1d63967e0a16671d7200f50e6211d3dd5b83c3d5
hajaking <953258481@qq.com>
2018-10-11 14:53:39 +0800

    Update lab1/lab1_3.py

--- lab1/lab1_3.py
+++ lab1/lab1_3.py
@@ -1,18 +1,16 @@
-from collections import deque
+from  collections import deque
 
+def search(lines,pattern,history=5):
+    previous_lines= deque(maxlen=history)
+    for line in lines:
+        for pattern in line:
+            yield line,previous_lines
+        previous_lines.append(line)
 
-def search(lines, pattern, history=5):
-    previous_lines = deque(maxlen=history)
-    for li in lines:
-        if pattern in li:
-            yield li, previous_lines
-        previous_lines.append(li)
-
-# Example use on a file
-if __name__ == '__main__':
+if __name__ =="__main__":
     with open(r'somefile.txt') as f:
-        for line, prevlines in search(f, 'Python', 5):
+        for line,prevlines in search(f,'Python', 5):
             for pline in prevlines:
-                print(pline, end='')
-            print(line, end='')
-            print('-' * 20)
+                print(pline,end='')
+            print(line,end='')
+            print('-'*20)
