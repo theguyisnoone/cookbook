e1b8222c71ef0ddf8bf00d5823e0530682fbfbff
hajaking <953258481@qq.com>
2018-10-11 12:08:45 +0800

    Update lab1/lab1_2.py
    fix the avg problem

--- lab1/lab1_2.py
+++ lab1/lab1_2.py
@@ -1,9 +1,10 @@
 '''解压可迭代对象赋值给多个变量
 问题 '''
-# import math
-# def drop_first_last(grades):
-#     first,*middle,last=grades
-#     return avg(middle)#去除第一个和最后一个的平均值
+class test(object):
+    def drop_first_last(self,grades):
+        first,*middle,last=grades
+        avg=sum(middle)/len(middle)
+        print("avg without first and last:{}".format(avg))
 
 #test
 record = ('Dave', 'dave@example.com', '773-555-1212', '847-555-1212')
@@ -14,5 +15,6 @@ print("name:{},email:{},phone_number:{}".format(name,email,phone_number))
 
 
 
-# grades=(66,77,43,99,95)
-# print(drop_first_last(record)) #avg不是自带函数
+grades=(66,77,43,99,95)
+t1=test()
+t1.drop_first_last(grades)
